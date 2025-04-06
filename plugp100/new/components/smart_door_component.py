de0b1aad71bc8f999622e8783d449110acb37667
Daniel Connolly <daniel@dconnolly.com>
2025-04-06 14:09:06 +0200

    fix: handle different possibilities for state of is_open (#189)
    Co-authored-by: petretiandrea <petretiandrea@gmail.com>

--- plugp100/new/components/smart_door_component.py
+++ plugp100/new/components/smart_door_component.py
@@ -1,7 +1,10 @@
+import logging
 from typing import Any
 
 from plugp100.new.components.device_component import DeviceComponent
 
+
+_LOGGER = logging.getLogger("SmartDoorComponent")
 
 # TODO: get component_negotiation for Tapo Smart Door sensor
 # this class is actually too specific for SmartDoor, the component can be called open_closed
@@ -12,4 +15,9 @@ class SmartDoorComponent(DeviceComponent):
         self.is_open = False
 
     async def update(self, current_state: dict[str, Any] | None = None):
-        self.is_open = current_state["is_open"]
+        if "is_open" in current_state:
+            self.is_open = current_state["is_open"]
+        elif "open" in current_state:
+            self.is_open = current_state["open"]
+        else:
+            _LOGGER.warning("Open state not found in current state")
