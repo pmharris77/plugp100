02b0f345957b64aab84a9257f856bf80373f868d
petretiandrea <petretiandrea@gmail.com>
2026-03-13 08:43:17 +0100

    fix: optional energy info fields

---
+++ tests/unit/test_energy_info.py
@@ -0,0 +1,16 @@
+from plugp100.responses.energy_info import EnergyInfo
+
+
+def test_energy_info_missing_today_energy_returns_none():
+    energy_info = EnergyInfo(
+        {
+            "today_runtime": 12,
+            "month_runtime": 120,
+            "month_energy": 456,
+            "current_power": 78,
+        }
+    )
+
+    assert energy_info.today_energy is None
+    assert energy_info.month_energy == 456
+    assert energy_info.current_power == 78
